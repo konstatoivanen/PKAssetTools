00647c023f32c35778f8306583d97327ebf299b3
konsta toivanen <konsta.13c@gmail.com>
2024-09-19 00:44:15 +0300

    refactor pk draw infos to have library level packing.

--- Source/PKShaderInstancing.h
+++ Source/PKShaderInstancing.h
@@ -5,13 +5,13 @@
 
 namespace PKAssets::Shader::Instancing
 {
-    // NOTE that for optimization purposes PK_Draw uses uint4(material, transform, global meshlet submesh, userdata) internally (this saves a redundant struct descriptor)
+    // NOTE that for optimization purposes PKDrawInfo uses uint4(material, uniform scale, transform, global meshlet submesh, userdata) internally (this saves a redundant struct descriptor)
     constexpr const static char* Instancing_Standalone_GLSL =
         "#define PK_INSTANCING_ENABLED                                                                                                          \n"
-        "#define PK_Draw uint4                                                                                                                  \n"
         "layout(std430, set = 0, binding = 100) readonly restrict buffer pk_Instancing_Transforms { mat3x4 pk_Instancing_Transforms_Data[]; };  \n"
-        "layout(std430, set = 3, binding = 101) readonly restrict buffer pk_Instancing_Indices { PK_Draw pk_Instancing_Indices_Data[]; };       \n"
+        "layout(std430, set = 3, binding = 101) readonly restrict buffer pk_Instancing_Indices { uint4 pk_Instancing_Indices_Data[]; };         \n"
         "mat3x4 pk_ObjectToWorld;                                                                                                               \n"
+        "float pk_Instancing_UniformScale;                                                                                                      \n"
         "uint pk_Instancing_Material;                                                                                                           \n"
         "uint pk_Instancing_Transform;                                                                                                          \n"
         "uint pk_Instancing_Submesh;                                                                                                            \n"
@@ -21,6 +21,7 @@ namespace PKAssets::Shader::Instancing
         "   pk_ObjectToWorld[0] = subgroupBroadcastFirst(pk_ObjectToWorld[0]);                                                                  \n"
         "   pk_ObjectToWorld[1] = subgroupBroadcastFirst(pk_ObjectToWorld[1]);                                                                  \n"
         "   pk_ObjectToWorld[2] = subgroupBroadcastFirst(pk_ObjectToWorld[2]);                                                                  \n"
+        "   pk_Instancing_UniformScale = subgroupBroadcastFirst(pk_Instancing_UniformScale);                                                    \n"
         "   pk_Instancing_Material = subgroupBroadcastFirst(pk_Instancing_Material);                                                            \n"
         "   pk_Instancing_Transform = subgroupBroadcastFirst(pk_Instancing_Transform);                                                          \n"
         "   pk_Instancing_Submesh = subgroupBroadcastFirst(pk_Instancing_Submesh);                                                              \n"
@@ -28,24 +29,25 @@ namespace PKAssets::Shader::Instancing
         "}                                                                                                                                      \n"
         "void PK_INSTANCING_ASSIGN_LOCALS(uint index)                                                                                           \n"
         "{                                                                                                                                      \n"
-        "    PK_Draw draw = pk_Instancing_Indices_Data[index];                                                                                  \n"
-        "    pk_ObjectToWorld = pk_Instancing_Transforms_Data[draw.y];                                                                          \n"
-        "    pk_Instancing_Material = draw.x;                                                                                                   \n"
-        "    pk_Instancing_Transform = draw.y;                                                                                                  \n"
-        "    pk_Instancing_Submesh = draw.z;                                                                                                    \n"
-        "    pk_Instancing_Userdata = draw.w;                                                                                                   \n"
+        "    uint4 data = pk_Instancing_Indices_Data[index];                                                                                    \n"
+        "    pk_Instancing_UniformScale = unpackHalf2x16(data.x).y;                                                                             \n"
+        "    pk_Instancing_Material = data.x & 0xFFFFu;                                                                                         \n"
+        "    pk_Instancing_Transform = data.y;                                                                                                  \n"
+        "    pk_Instancing_Submesh = data.z;                                                                                                    \n"
+        "    pk_Instancing_Userdata = data.w;                                                                                                   \n"
+        "    pk_ObjectToWorld = pk_Instancing_Transforms_Data[pk_Instancing_Transform];                                                         \n"
         "}                                                                                                                                      \n";
 
     constexpr const static char* Instancing_Base_GLSL =
         "#define PK_INSTANCING_ENABLED                                                                                                                              \n"
-        "#define PK_Draw uint4                                                                                                                                      \n"
         "layout(std430, set = 0, binding = 100) readonly restrict buffer pk_Instancing_Transforms { mat3x4 pk_Instancing_Transforms_Data[]; };                      \n"
-        "layout(std430, set = 3, binding = 101) readonly restrict buffer pk_Instancing_Indices { PK_Draw pk_Instancing_Indices_Data[]; };                           \n"
+        "layout(std430, set = 3, binding = 101) readonly restrict buffer pk_Instancing_Indices { uint4 pk_Instancing_Indices_Data[]; };                             \n"
         "layout(std430, set = 3, binding = 102) readonly restrict buffer pk_Instancing_Properties { PK_MaterialPropertyBlock pk_Instancing_Properties_Data[]; };    \n"
         "layout(set = 3, binding = 103) uniform texture2D pk_Instancing_Textures2D[];                                                                               \n"
         "layout(set = 3, binding = 104) uniform texture3D pk_Instancing_Textures3D[];                                                                               \n"
         "layout(set = 3, binding = 105) uniform textureCube pk_Instancing_TexturesCube[];                                                                           \n"
         "mat3x4 pk_ObjectToWorld;                                                                                                                                   \n"
+        "float pk_Instancing_UniformScale;                                                                                                                          \n"
         "uint pk_Instancing_Material;                                                                                                                               \n"
         "uint pk_Instancing_Transform;                                                                                                                              \n"
         "uint pk_Instancing_Submesh;                                                                                                                                \n"
@@ -55,6 +57,7 @@ namespace PKAssets::Shader::Instancing
         "   pk_ObjectToWorld[0] = subgroupBroadcastFirst(pk_ObjectToWorld[0]);                                                                                      \n"
         "   pk_ObjectToWorld[1] = subgroupBroadcastFirst(pk_ObjectToWorld[1]);                                                                                      \n"
         "   pk_ObjectToWorld[2] = subgroupBroadcastFirst(pk_ObjectToWorld[2]);                                                                                      \n"
+        "   pk_Instancing_UniformScale = subgroupBroadcastFirst(pk_Instancing_UniformScale);                                                                        \n"
         "   pk_Instancing_Material = subgroupBroadcastFirst(pk_Instancing_Material);                                                                                \n"
         "   pk_Instancing_Transform = subgroupBroadcastFirst(pk_Instancing_Transform);                                                                              \n"
         "   pk_Instancing_Submesh = subgroupBroadcastFirst(pk_Instancing_Submesh);                                                                                  \n"
@@ -62,13 +65,14 @@ namespace PKAssets::Shader::Instancing
         "}                                                                                                                                                          \n"
         "void PK_INSTANCING_ASSIGN_LOCALS(uint index)                                                                                                               \n"
         "{                                                                                                                                                          \n"
-        "    PK_Draw draw = pk_Instancing_Indices_Data[index];                                                                                                      \n"
-        "    pk_ObjectToWorld = pk_Instancing_Transforms_Data[draw.y];                                                                                              \n"
-        "    pk_Instancing_Material = draw.x;                                                                                                                       \n"
-        "    pk_Instancing_Transform = draw.y;                                                                                                                      \n"
-        "    pk_Instancing_Submesh = draw.z;                                                                                                                        \n"
-        "    pk_Instancing_Userdata = draw.w;                                                                                                                       \n"
-        "    PK_MaterialPropertyBlock prop = pk_Instancing_Properties_Data[draw.x];                                                                                 \n";
+        "    uint4 data = pk_Instancing_Indices_Data[index];                                                                                                        \n"
+        "    pk_Instancing_UniformScale = unpackHalf2x16(data.x).y;                                                                                                 \n"
+        "    pk_Instancing_Material = data.x & 0xFFFFu;                                                                                                             \n"
+        "    pk_Instancing_Transform = data.y;                                                                                                                      \n"
+        "    pk_Instancing_Submesh = data.z;                                                                                                                        \n"
+        "    pk_Instancing_Userdata = data.w;                                                                                                                       \n"
+        "    pk_ObjectToWorld = pk_Instancing_Transforms_Data[pk_Instancing_Transform];                                                                             \n"
+        "    PK_MaterialPropertyBlock prop = pk_Instancing_Properties_Data[pk_Instancing_Material];                                                                 \n";
 
     constexpr const static char* Instancing_Stage_GLSL = "\nPK_INSTANCING_ASSIGN_STAGE_LOCALS \n";
 
