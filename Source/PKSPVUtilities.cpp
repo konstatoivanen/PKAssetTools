c2bc5e2287c4e56d530c2a5b5d2b3783b385ca6a
konsta toivanen <konsta.13c@gmail.com>
2026-05-04 17:55:26 +0300

    refactor spv resource write reflection to support fixed size image arrays.

--- Source/PKSPVUtilities.cpp
+++ Source/PKSPVUtilities.cpp
@@ -29,7 +29,7 @@ namespace PKAssets::Shader
         uint32_t base;
     };
 
-    static bool ReflectAccessChainStore(const uint32_t* code, const uint32_t wordCount, uint32_t accessChainId)
+    static bool FindOpConstUint(const uint32_t* code, const uint32_t wordCount, uint32_t constId, uint32_t* outValue)
     {
         uint32_t i = 5u; // Bytecode starts at word index 5, after the header.
 
@@ -39,12 +39,78 @@ namespace PKAssets::Shader
             uint16_t wcount = (code[i] >> 16) & 0xFFFFu;
             const uint32_t* params = code + i + 1;
 
+            if (opcode == SpvOp::SpvOpConstant && params[1] == constId)
+            {
+                *outValue = params[2];
+                return true;
+            }
+
+            i += wcount;
+        }
+
+        return false;
+    }
+
+    bool ReflectResourceWrite(const uint32_t* code, const uint32_t wordCount, uint32_t variable, PKDescriptorType type)
+    {
+        if (type != PKDescriptorType::Image && 
+            type != PKDescriptorType::StorageBuffer && 
+            type != PKDescriptorType::DynamicStorageBuffer)
+        {
+            return false;
+        }
+
+        uint32_t i = 5u; // Bytecode starts at word index 5, after the header.
+
+        uint32_t accessChainId = ~0u;
+        uint32_t opLoadId = ~0u;
+
+        while (i < wordCount)
+        {
+            SpvOp opcode = (SpvOp)(code[i] & 0xFFFFu);
+            uint16_t wcount = (code[i] >> 16) & 0xFFFFu;
+            const uint32_t* params = code + i + 1u;
+
+            // Reset access chain if the variable was reused.
+            if (opcode == SpvOp::SpvOpAccessChain && params[1] == accessChainId)
+            {
+                accessChainId = ~0u;
+            }
+
+            // Reset opload if the variable was reused.
+            if (opcode == SpvOp::SpvOpLoad && params[1] == opLoadId)
+            {
+                opLoadId = ~0u;
+            }
+
+            // found access chain. potentially aliased resource
+            if (opcode == SpvOp::SpvOpAccessChain && params[2] == variable)
+            {
+                accessChainId = params[1];
+            }
+
+            if (opcode == SpvOp::SpvOpLoad && (params[2] == variable || params[2] == accessChainId))
+            {
+                opLoadId = params[1];
+            }
+
+            // We have a valid access chain from resource to a load op. look for write op.
+            // Buffer access always references resoruces through access chains so we can ignore the op load.
             switch (opcode)
             {
                 case SpvOp::SpvOpStore:
                 case SpvOp::SpvOpAtomicStore:
                 {
                     if (params[0] == accessChainId)
+                    {
+                        return true;
+                    }
+                }
+                break;
+
+                case SpvOp::SpvOpImageWrite:
+                {
+                    if (params[0] == opLoadId)
                     {
                         return true;
                     }
@@ -79,110 +145,6 @@ namespace PKAssets::Shader
         }
 
         return false;
-    }
-
-    static bool ReflectOpLoadImageWrite(const uint32_t* code, const uint32_t wordCount, uint32_t opLoadId)
-    {
-        uint32_t i = 5u; // Bytecode starts at word index 5, after the header.
-
-        while (i < wordCount)
-        {
-            SpvOp opcode = (SpvOp)(code[i] & 0xFFFFu);
-            uint16_t wcount = (code[i] >> 16) & 0xFFFFu;
-            const uint32_t* params = code + i + 1;
-
-            if (opcode == SpvOp::SpvOpImageWrite &&
-                params[0] == opLoadId)
-            {
-                return true;
-            }
-
-            i += wcount;
-        }
-
-        return false;
-    }
-
-    static bool FindOpConstUint(const uint32_t* code, const uint32_t wordCount, uint32_t constId, uint32_t* outValue)
-    {
-        uint32_t i = 5u; // Bytecode starts at word index 5, after the header.
-
-        while (i < wordCount)
-        {
-            SpvOp opcode = (SpvOp)(code[i] & 0xFFFFu);
-            uint16_t wcount = (code[i] >> 16) & 0xFFFFu;
-            const uint32_t* params = code + i + 1;
-
-            if (opcode == SpvOp::SpvOpConstant && params[1] == constId)
-            {
-                *outValue = params[2];
-                return true;
-            }
-
-            i += wcount;
-        }
-
-        return false;
-    }
-
-    bool ReflectBufferWrite(const uint32_t* code, const uint32_t wordCount, uint32_t variable)
-    {
-        uint32_t i = 5u; // Bytecode starts at word index 5, after the header.
-
-        while (i < wordCount)
-        {
-            SpvOp opcode = (SpvOp)(code[i] & 0xFFFFu);
-            uint16_t wcount = (code[i] >> 16) & 0xFFFFu;
-            const uint32_t* params = code + i + 1;
-
-            if (opcode == SpvOp::SpvOpAccessChain &&
-                params[2] == variable &&
-                ReflectAccessChainStore(code, wordCount, params[1]))
-            {
-                return true;
-            }
-
-            i += wcount;
-        }
-
-        return false;
-    }
-
-    bool ReflectImageWrite(const uint32_t* code, const uint32_t wordCount, uint32_t variable)
-    {
-        uint32_t i = 5u; // Bytecode starts at word index 5, after the header.
-
-        while (i < wordCount)
-        {
-            SpvOp opcode = (SpvOp)(code[i] & 0xFFFFu);
-            uint16_t wcount = (code[i] >> 16) & 0xFFFFu;
-            const uint32_t* params = code + i + 1;
-
-            if (opcode == SpvOp::SpvOpLoad &&
-                params[2] == variable &&
-                ReflectOpLoadImageWrite(code, wordCount, params[1]))
-            {
-                return true;
-            }
-
-            i += wcount;
-        }
-
-        return false;
-    }
-
-    bool ReflectResourceWrite(const uint32_t* code, const uint32_t wordCount, uint32_t variable, PKDescriptorType type)
-    {
-        switch (type)
-        {
-            case PKDescriptorType::Image:
-                return ReflectImageWrite(code, wordCount, variable);
-            case PKDescriptorType::StorageBuffer:
-            case PKDescriptorType::DynamicStorageBuffer:
-                return ReflectBufferWrite(code, wordCount, variable);
-            default: 
-                return false;
-        }
     }
 
     bool ReflectLocalSize(const uint32_t* code, const uint32_t wordCount, uint32_t* localSize)
--- Source/PKSPVUtilities.h
+++ Source/PKSPVUtilities.h
@@ -3,8 +3,6 @@
 
 namespace PKAssets::Shader
 {
-    bool ReflectBufferWrite(const uint32_t* code, const uint32_t wordCount, uint32_t variable);
-    bool ReflectImageWrite(const uint32_t* code, const uint32_t wordCount, uint32_t variable);
     bool ReflectResourceWrite(const uint32_t* code, const uint32_t wordCount, uint32_t variable, PKDescriptorType type);
     bool ReflectLocalSize(const uint32_t* code, const uint32_t wordCount, uint32_t* outLocalSize);
 }
